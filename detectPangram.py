d0bd2862ffdcaae82a9076cf660ed5c3ffed568b
Kelishia Radhalal <kelishiaradhalal@Kelishias-MacBook-Pro.local>
2023-03-23 21:09:43 +0200

    add check pangram functionality

--- detectPangram.py
+++ detectPangram.py
@@ -1,13 +1,22 @@
 import string
 
 # set is a collection of unique elements
-letters=set(string.ascii_lowercase)
+alphabet=string.ascii_lowercase
+
+def checkPangram(pangram):
+    for letter in alphabet:
+        if letter not in pangram.lower():
+            return False
+    return True
 
 with open('data/pangrams.txt', 'r') as f:
     for line in f:
         pangram=line.strip()
         for character in string.punctuation:
             pangram = pangram.replace(character, '')
-    
-        print(pangram)
+        print(pangram+": ")
+        if (checkPangram(pangram) == True):
+            print("yes\n")
+        else:
+            print("no\n")
     f.close()
